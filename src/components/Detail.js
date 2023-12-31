b1a18a712662bfb9a7539afbe819adda34a4613a
Anusha <anushasharma824@gmail.com>
2023-12-31 22:20:19 +0530

    exercise videos completed

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,22 +5,23 @@ import TargetImage from "../assets/icons/target.png";
 import EquipmentImage from "../assets/icons/equipment.png";
 
 const Detail = ({ exerciseDetail }) => {
-  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  const { bodyPart, gifUrl, name, target, equipment, instructions } =
+    exerciseDetail;
 
   const extraDetail = [
     {
-        icon: BodyPartImage,
-        name: bodyPart,
+      icon: BodyPartImage,
+      name: bodyPart,
     },
     {
-        icon: TargetImage,
-        name: target,
+      icon: TargetImage,
+      name: target,
     },
     {
-        icon: EquipmentImage,
-        name: equipment,
+      icon: EquipmentImage,
+      name: equipment,
     },
-  ]
+  ];
 
   return (
     <Stack
@@ -30,21 +31,33 @@ const Detail = ({ exerciseDetail }) => {
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
         <Typography variant="h3">{name}</Typography>
-        <Typography variant="h6">
-          Exercises keep you strong. {` `}
-          {name} {` `} is one of the best exercises
-          to target your {target}. It will help you improve your mood and gain
-          energy.
-        </Typography>
-        {extraDetail.map((item) => (
-            <Stack key={item.name} direction="row" gap="24px" alignItems="center">
-                <Button sx={{background: '#FFF2DB', borderRadius: '50%', width: '100px', height: '100px'}}>
-                    <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }}/>
-                </Button>
-                <Typography textTransform="capitalize" variant="h5">
-                    {item.name}
-                </Typography>
-            </Stack>
+        <Typography variant="h4">Instructions</Typography>
+        {instructions?.map((step, index) => (
+          <Typography key={index} padding={0} margin={0}>
+            {index + 1}. {step}
+          </Typography>
+        ))}
+
+        {extraDetail.map((item, id) => (
+          <Stack key={id} direction="row" gap="24px" alignItems="center">
+            <Button
+              sx={{
+                background: "#FFF2DB",
+                borderRadius: "50%",
+                width: "80px",
+                height: "80px",
+              }}
+            >
+              <img
+                src={item.icon}
+                alt={bodyPart}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Button>
+            <Typography textTransform="capitalize" variant="h6">
+              {item.name}
+            </Typography>
+          </Stack>
         ))}
       </Stack>
     </Stack>
